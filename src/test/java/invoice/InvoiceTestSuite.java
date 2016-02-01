4bbf2d48ecdea9d5b7f0c33a53a6209b132893e0
Fallbrawl <herdemismyrise.bt@gmail.com>
2016-02-01 12:01:15 +0200

    plo

--- src/test/java/invoice/InvoiceTestSuite.java
+++ src/test/java/invoice/InvoiceTestSuite.java
@@ -3,11 +3,17 @@ package invoice;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import static org.testng.Assert.assertTrue;
 
@@ -20,8 +26,19 @@ public class InvoiceTestSuite {
     @BeforeTest
 
     public void setUp() {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setJavascriptEnabled(true);
+        caps.setCapability("takesScreenshot", true);
+        caps.setCapability("screen-resolution", "1280x720");
+        //caps.setCapability("phantomjs.page.settings.userAgent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36");
+        //caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "C:\\Users\\USER\\Downloads\\phantomjs-2.0.0-windows\\bin\\phantomjs.exe");
 
-        driver = new FirefoxDriver();
+        ArrayList<String> cliArgsCap = new ArrayList<String>();
+        cliArgsCap.add("--webdriver-loglevel=NONE");
+        caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, cliArgsCap);
+        Logger.getLogger(PhantomJSDriverService.class.getName()).setLevel(Level.OFF);
+
+        driver = new PhantomJSDriver(caps);
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.get("http://invoicedev.php.attractgroup.com/public/login");
 

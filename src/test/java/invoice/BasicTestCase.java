b5c214b3e8e0fd4ef43394072ec1300036e9c070
Fallbrawl <herdemismyrise.bt@gmail.com>
2016-02-05 14:47:09 +0200

    plo

--- src/test/java/invoice/BasicTestCase.java
+++ src/test/java/invoice/BasicTestCase.java
@@ -2,6 +2,7 @@ package invoice;
 
 import com.invoice.data.UserData;
 import com.invoice.utils.ConfigProperties;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
@@ -17,6 +18,9 @@ public class BasicTestCase {
     protected static WebDriver driver;
 
     public UserData admin = new UserData("pavel.a@attractgroup.com", "attract");
+    public UserData manager_buyer = new UserData("pavel.a.manager.buyer@attractgroup.com", "attract");
+    public UserData manager_seller = new UserData("pavel.a.manager_sales@attractgroup.com", "attract");
+    public UserData accounter = new UserData("pavel.a.accounter@attractgroup.com", "attract");
 
     protected WebDriver getWebDriver() {
         if (driver == null) {
